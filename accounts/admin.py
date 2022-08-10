8b2e29d3cfa84e58c5c7b6fd0577bcf73688903a
Dinesh Bodala <ds176.edu@gmail.com>
2022-08-10 20:59:27 +0530

    Adding whole Database

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models  import customer
 from .models  import product
-from .models import order
+from .models import order, tag
 
 admin.site.register(customer)
 admin.site.register(product)
 admin.site.register(order)
+admin.site.register(tag)
 # Register your models here.
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from re import T
 from sre_constants import CATEGORY
 from unicodedata import category
 from django.db import models
@@ -10,21 +11,38 @@ class customer(models.Model):
     date_created=models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self) -> str:
-        return self.name
+        return self.name  
+
+class tag(models.Model):
+    name=models.CharField(max_length=250, null=True)
+
+    def __str__(self) -> str:
+        return self.name  
+
+    
+
 
 class product(models.Model):
     CATEGORY=(('Indoor', 'Indoor'), ('Outdoor','OutDoor'))
     name=models.CharField(max_length=250, null=True)
     price=models.FloatField(null=True)
-    category=models.CharField(max_length=250, null=True)
-    description=models.CharField(max_length=250, null=True)
+    category=models.CharField(max_length=250, null=True, choices=CATEGORY)
+    description=models.CharField(max_length=250, null=True, blank=True)
     date_created=models.DateTimeField(auto_now_add=True, null=True)
+    Tag=models.ManyToManyField(tag)
+
+    def __str__(self) -> str:
+        return self.name  
+
 
 
 class order(models.Model):
     STATUS=(('Pending', 'Pending'),('Delivered', 'Delivered'),('Out For Delivery','Out For Delivery'))
+    Customer=models.ForeignKey(customer, null=True, on_delete=models.SET_NULL)
+    Product=models.ForeignKey(product, null=True, on_delete=models.SET_NULL)
     date_created=models.DateTimeField(auto_now_add=True,  null=True)
-    status=models.CharField(max_length=250, null=True)
+    status=models.CharField(max_length=250, null=True, choices=STATUS)
+
 
 
 
